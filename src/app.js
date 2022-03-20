3b9bf151fefe2537aa30b3d77a66ebf0023cc80b
ddlmar <ddlmar13@gmail.com>
2022-03-20 17:21:11 -0300

    aplicando api de login

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
-
 const formScreen = {
     render: () => {
         return `
             <div class="formContainer">
                 <h1 class="loginTitle">Login<h1>
-                <input class="inputArea" type="text" name="email" placeholder="Seu e-mail..." ></input>   
-                <span class="inputSplit"></span>
-                <input class="inputArea" type="password" name="password" placeholder="Sua senha..." ></input>    
-                <span class="inputSplit"></span>
-                <button class= "submitButton">Login</button>
-                <a class= "loginReset" href="/">Esqueci a senha</a>
+                <div>
+                
+                    <input class="inputArea" id="email" type="text" name="email" placeholder="Your email..." ></input>   
+                    <span class="inputSplit"></span>
+                    <input class="inputArea" id="password" type="password" name="password" placeholder="Your password..." ></input>    
+                    <span class="inputSplit"></span>
+                    <button class="submitButton" id= "submitButton">Login</button>
+                    <p class="loginResponse" id="loginResponse"></p>
+                    <a class="loginReset" href="/">Forgot the password</a>
+                
+                </div>
             </div>
         `
     }
--- src/renderScreen.js
+++ src/renderScreen.js
@@ -1,8 +1,75 @@
 import formScreen from "./app.js";
 
+const URL = "https://reqres.in/api/"
+
+// async function listUsers(page = 1) {
+//     const query = URL + 'users?page=' + page
+//     const response = await fetch(query, 
+//         {
+//             method: 'GET', 
+//             headers: {
+//                 'Content-Type': 'application/json' 
+//             },
+//         })
+//     const json = await response.json()
+//     return json.data
+// }
+
+async function login(email, password) {
+    const url = URL + 'login'
+    const request = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        }, 
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    }
+    const response = await fetch(url, request);
+    const json = response.json();
+    return json;
+}
+
+// async function register(email, password) {
+//     const url = URL + 'register'
+//     const request = {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json' 
+//         }, 
+//         body: JSON.stringify({
+//             email: email,
+//             password: password
+//         })
+//     }
+//     const response = await fetch(url, request)
+//     const json = response.json()
+//     return json
+// }
+
 const router = () => {
     const screen = document.getElementById("mainContainer");
     screen.innerHTML = formScreen.render();
+    
+    const submitButton = document.getElementById("submitButton");
+    const emailField = document.getElementById("email");
+    const passwordField = document.getElementById("password");
+    
+    submitButton.addEventListener('click', async () => {
+        let loginResponse = await login(emailField.value, passwordField.value);
+        const spanElement = document.getElementById('loginResponse');
+        if (loginResponse.error == "Missing email or username") {
+            spanElement.innerHTML = 'Incorret email or password';
+            spanElement.style.color = '#e7473c';
+            spanElement.style.animation = '0.1s linear 3 alternate slidein';  
+        }
+        if (loginResponse.token == 'QpwL5tke4Pnpja7X4') {
+            spanElement.style.color = '#303030';
+            spanElement.innerHTML = 'Login succesful';
+        }
+    })
 };
 
 window.addEventListener("load", router);
